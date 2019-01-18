cc8b8d7b71c0a202a2c09c4706666bb1a169dc2d
Michael Wrona <michael.wrona77@gmail.com>
2019-01-18 15:13:47 -0600

    Added vectNormP.py

---
+++ vectNormP.py
@@ -0,0 +1,38 @@
+"""
+
+VECTORNORMP: linalg-python
+Calculate the p-norm of a vector
+
+By: Michael Wrona
+Student, B.S. Aerospace Engineering
+Iowa State University (Ames, IA)
+
+"""
+
+
+import numpy as np
+
+
+def vectNormP(x, p):
+    """
+    CALCULATE THE 'p' NORM OF A VECTOR
+    'x': Vector to calculate the norm of
+    'p': TAKEN AS 2 NORMALLY (p-norm), no pun intended ;)
+    **NOTE: 'x' MUST BE NUMPY ARRAY
+    >>> x = np.array([1, 2, 3])
+    >>> vectNorm(x, 1)
+    >>> 6
+    """
+    # Check if input is Numpy array
+    if type(x) != np.ndarray:
+        print("INPUT IS NOT NUMPY ARRAY IN ROUTINE 'vectNorm()'")
+        return
+
+    n = len(x)
+
+    # Raise each element to the power of 'p', find the sum of the elements
+    sum = 0.0
+    for i in range(n):
+        sum += abs(x[i]) ** p
+
+    return pow(sum, (1 / p))  # Raise sum to the power of 1/p
